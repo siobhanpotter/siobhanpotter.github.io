496471073aaca97f0eea99ce969971251c5d009b
siobhanpotter <siobhanpotterone@gmail.com>
2018-01-21 00:15:43 +0000

    added styling to cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,9 +21,10 @@ const Projects = () => {
               <div className="img-container">
               <img className="card-img-top" src={musicBox} alt="Card image cap"></img>
               </div>
-              <div className="card-body">
+              <div className="card-body grow">
                 <h5 className="card-title">Music Box</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">An app for musicians to find other musicians or bands to play with in their local area. I came up with the idea because of my passion for learning and playing music with others. This is a MERN stack application and makes use of the Google Map and Google Places API.
+                </p>
                 <button type="button" class="btn btn-outline-info">Launch App</button>
                 <button type="button" class="btn btn-outline-info">View Code</button>
               </div>
@@ -35,7 +36,7 @@ const Projects = () => {
               </div>
               <div className="card-body">
                 <h5 className="card-title">Gig.ly</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">A social music event app designed for people to chat within groups attending upcoming gigs. This was a team project that involved using Github collaboratively to produce a RESTful MEAN stack app. It combines data from the Ticketmaster and Spotify API to pull in event info and the artists top tracks.</p>
                 <button type="button" class="btn btn-outline-info">Launch App</button>
                 <button type="button" class="btn btn-outline-info">View Code</button>
               </div>
@@ -60,7 +61,7 @@ const Projects = () => {
               </div>
               <div className="card-body">
                 <h5 className="card-title">Viewr</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">A platform for the public to add and review exhibitions they had visited. My background in photography led me to come up with the idea for this app. I utilized the  CRUD/RESTful concepts when creating my first full stack app, getting to know how to use MongoDB and make requests to my API. </p>
                 <button type="button" class="btn btn-outline-info">Launch App</button>
                 <button type="button" class="btn btn-outline-info">View Code</button>
               </div>
@@ -72,7 +73,7 @@ const Projects = () => {
               </div>
               <div className="card-body">
                 <h5 className="card-title">Unjmuble This</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">The idea of the game is to un-jumble words within a specific time frame, the game becomes more challenging as it progresses and the speed increases. I made this game on week three of the immersive course. For this project, I wanted to consolidate my knowledge of Javascript, jQuery and CSS. </p>
                 <button type="button" class="btn btn-outline-info">Launch App</button>
                 <button type="button" class="btn btn-outline-info">View Code</button>
               </div>
