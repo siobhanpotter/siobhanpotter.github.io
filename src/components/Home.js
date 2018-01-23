57f2feccc6bfb85fdbb55db945706068ed2ebfdc
siobhanpotter <siobhanpotterone@gmail.com>
2018-01-23 13:16:41 +0000

    changed color of heading

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import '../scss/style.scss';
 // import logoOutline from '../assets/OUTLINE.png';
-// import logoGrey from '../assets/GREY.png';
+import logoGrey from '../assets/GREY.png';
 
 const Home = () => {
 
@@ -10,13 +10,13 @@ const Home = () => {
     <div id="Home">
       <section className="header">
         <div className="col-12  title">
-          {/* <img className="img-responsive logo-outline" src={logoOutline}></img> */}
           <div className="title-name">
             <h4>SIOBHAN POTTER</h4>
             <h6>Web Developer | Creative | Music Lover</h6>
           </div>
         </div>
-        <div className="intro">
+        {/* <img className="img-responsive logo-outline" src={logoGrey}></img> */}
+        <div className="intro fade-in one">
           <p className="element"><span>Hi, I'm Siobhan,</span><br/> I'm a web developer   based in London. Check out my work, find out more about me & get in   touch.</p>
         </div>
       </section>
